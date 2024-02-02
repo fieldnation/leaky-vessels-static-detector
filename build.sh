a326cf44467c851b4707534c132b3fde5477a770
Ian Loverink <ian.loverink@fieldnation.com>
2024-02-02 09:08:49 -0600

    Build x86_64

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-docker pull golang:1.21.6-alpine3.19
-docker run --rm -v "$PWD":/usr/src/app -w /usr/src/app golang:1.21.6-alpine3.19 go build -v
+docker pull --platform=linux/x86_64 golang:1.21.6-alpine3.19
+docker run --platform=linux/x86_64 --rm -v "$PWD":/usr/src/app -w /usr/src/app golang:1.21.6-alpine3.19 go build -v
