2bb08f5818d8e227f3faa4e8052d404439a28539
Ian Loverink <ian.loverink@fieldnation.com>
2024-02-02 08:40:30 -0600

    Set loglevel and ignore idea

--- internal/common/logger.go
+++ internal/common/logger.go
@@ -13,59 +13,59 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
- 
+
 package common
 
 import (
-    "os"
+	"os"
 	"runtime"
 
-    "github.com/sirupsen/logrus"
+	"github.com/sirupsen/logrus"
 )
 
 const PREFIX = "[ + ] "
 
 type CustomFormatter struct {
-    logrus.Formatter
+	logrus.Formatter
 }
 
 func (f *CustomFormatter) Format(entry *logrus.Entry) ([]byte, error) {
-    // Get the function name.
-    if pc, _, _, ok := runtime.Caller(6); ok {
-        if fn := runtime.FuncForPC(pc); fn != nil {
+	// Get the function name.
+	if pc, _, _, ok := runtime.Caller(6); ok {
+		if fn := runtime.FuncForPC(pc); fn != nil {
 			entry.Message = PREFIX + entry.Message
-        }
-    }
+		}
+	}
 
-    // Use the embedded formatter to format the entry.
-    return f.Formatter.Format(entry)
+	// Use the embedded formatter to format the entry.
+	return f.Formatter.Format(entry)
 }
 
-func init(){
+func init() {
 	Initialize()
 }
 
 // Initialize sets up the logger configuration.
 func Initialize() {
-    // Set the log output format to TextFormatter with color enabled.
-    logrus.SetFormatter(&CustomFormatter{
-        Formatter: &logrus.TextFormatter{
-            FullTimestamp: true,
-        },
-    })
+	// Set the log output format to TextFormatter with color enabled.
+	logrus.SetFormatter(&CustomFormatter{
+		Formatter: &logrus.TextFormatter{
+			FullTimestamp: true,
+		},
+	})
 
-    // Output to stderr.
-    logrus.SetOutput(os.Stderr)
+	// Output to stderr.
+	logrus.SetOutput(os.Stderr)
 
-    // Set the log level.
-    logrus.SetLevel(logrus.InfoLevel)
+	// Set the log level.
+	logrus.SetLevel(logrus.WarnLevel)
 }
 
 // Get returns an instance of the logger.
 func GetLogger() *logrus.Logger {
-    return logrus.StandardLogger()
+	return logrus.StandardLogger()
 }
 
 func EnableDebug() {
-    logrus.SetLevel(logrus.DebugLevel)
+	logrus.SetLevel(logrus.DebugLevel)
 }
